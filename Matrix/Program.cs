7a713d80cb6c2a6418df7a84b8ab4133688433c2
Funt <funt_88@mail.ru>
2022-12-05 20:41:16 +0300

    лекции 4 часть

---
+++ Matrix/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вывести случайные числа в случайных точках.
+
+Random r = new Random();
+Console.CursorVisible = false;
+while (true)
+{
+    Console.SetCursorPosition(
+        left: r.Next(Console.WindowWidth),
+        top: r.Next(Console.WindowHeight)
+    );
+Console.Write(r.Next(10));
+Thread.Sleep(1000);
+}
